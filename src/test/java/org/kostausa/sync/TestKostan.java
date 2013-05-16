7b14362042a1b3122def0a25f09536e0b1c0037d
Eun-Gyu Kim <eungyu.kim@gmail.com>
2013-05-15 21:49:39 -0700

    Added TestKostan.java to test various malformated input

--- src/test/java/org/kostausa/sync/TestKostan.java
+++ src/test/java/org/kostausa/sync/TestKostan.java
@@ -1,19 +1,97 @@
 package org.kostausa.sync;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-public class TestKostan {
+@RunWith(JUnit4.class)
+@SuppressWarnings("unused")
+public class TestKostan 
+{
 
+  @Rule 
+  public ExpectedException exception = ExpectedException.none();
+  
+  private String _name;
+  private String _gender;
+  private String _email;
+  
   @Before
   public void setUp() throws Exception {
+    _name  = "코스타";
+    _gender = "M";
+    _email = "web@kostausa.org";
   }
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void testGender() throws IncompleteRecordException
+  {
+    Kostan person1 = new Kostan(Conference.INDIANAPOLIS, _name, "여", _email);
+    assertEquals(person1.getGender(), "F");
+    Kostan person2 = new Kostan(Conference.INDIANAPOLIS, _name, "남", _email);        
+    assertEquals(person2.getGender(), "M");
+  }
+  
+  @Test
+  public void testNullName() throws IncompleteRecordException
+  {
+    exception.expect(IncompleteRecordException.class);
+    Kostan person = new Kostan(Conference.INDIANAPOLIS, null, _gender, _email);
   }
 
+  @Test
+  public void testEmptyName() throws IncompleteRecordException
+  {
+    exception.expect(IncompleteRecordException.class);
+    Kostan person1 = new Kostan(Conference.INDIANAPOLIS, "", _gender, _email);
+
+    exception.expect(IncompleteRecordException.class);
+    Kostan person2 = new Kostan(Conference.INDIANAPOLIS, "   ", _gender, _email);
+  }
+
+  @Test
+  public void testNullGender() throws IncompleteRecordException
+  {
+    exception.expect(IncompleteRecordException.class);
+    Kostan person = new Kostan(Conference.INDIANAPOLIS, _name, null, _email);
+  }  
+
+  @Test
+  public void testNullEmail() throws IncompleteRecordException
+  {
+    exception.expect(IncompleteRecordException.class);
+    Kostan person = new Kostan(Conference.INDIANAPOLIS, _name, _gender, null);
+  } 
+  
+  @Test
+  public void testInvalidEmails() throws IncompleteRecordException
+  {
+    exception.expect(IncompleteRecordException.class);
+    Kostan person1 = new Kostan(Conference.INDIANAPOLIS, _name, _gender, "1234");    
+
+    exception.expect(IncompleteRecordException.class);
+    Kostan person2 = new Kostan(Conference.INDIANAPOLIS, _name, _gender, "1@1");    
+
+    exception.expect(IncompleteRecordException.class);
+    Kostan person3 = new Kostan(Conference.INDIANAPOLIS, _name, _gender, "asdf@asdf");        
+  }
+
+  @Test
+  public void testTrim() throws IncompleteRecordException
+  {
+    Kostan person1 = new Kostan(Conference.INDIANAPOLIS, _name, _gender, "  web@kostausa.org  \t ");        
+    assertEquals(person1.getEmail(), _email);
+
+    Kostan person2 = new Kostan(Conference.INDIANAPOLIS, _name, "  여 \n", _email);        
+    assertEquals(person2.getGender(), "F");
+    
+    Kostan person3 = new Kostan(Conference.INDIANAPOLIS, " 김 은   규 ", _gender, _email);
+    assertEquals(person3.getName(), "김은규");
+    
+  }
 }
