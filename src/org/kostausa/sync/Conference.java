de6540e754948145094a3b0a46c93dcc28855a22
Eun-Gyu Kim <eungyu.kim@gmail.com>
2013-05-15 20:34:55 -0700

    Fun with comments

--- src/org/kostausa/sync/Conference.java
+++ src/org/kostausa/sync/Conference.java
@@ -8,8 +8,8 @@ package org.kostausa.sync;
  */
 public enum Conference 
 {
-  CHICAGO(0),
-  INDIANAPOLIS(1);
+  CHICAGO(0),         // 25-And Up 을 위한 대학원 및 청년 컨퍼런스
+  INDIANAPOLIS(1);    // 18-25세를 위한 학부생 컨퍼런스
   
   private final int _num;
   
